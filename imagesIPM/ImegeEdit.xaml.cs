a647c15683259993d9dfec109a27c77e01494cc3
kk90 <kr.korycki@gmail.com>
2015-02-09 21:02:45 +0100

    edit page view and view model

--- imagesIPM/ImegeEdit.xaml.cs
+++ imagesIPM/ImegeEdit.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using imagesIPM.Common;
+using imagesIPM.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -6,12 +7,14 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
 // The Basic Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234237
@@ -26,19 +29,15 @@ namespace imagesIPM
 
         private NavigationHelper navigationHelper;
         private ObservableDictionary defaultViewModel = new ObservableDictionary();
+        private ImageEditorViewModel vm;
 
-        /// <summary>
-        /// This can be changed to a strongly typed view model.
-        /// </summary>
+
         public ObservableDictionary DefaultViewModel
         {
             get { return this.defaultViewModel; }
         }
 
-        /// <summary>
-        /// NavigationHelper is used on each page to aid in navigation and 
-        /// process lifetime management
-        /// </summary>
+
         public NavigationHelper NavigationHelper
         {
             get { return this.navigationHelper; }
@@ -51,49 +50,39 @@ namespace imagesIPM
             this.navigationHelper = new NavigationHelper(this);
             this.navigationHelper.LoadState += navigationHelper_LoadState;
             this.navigationHelper.SaveState += navigationHelper_SaveState;
+
+            vm = new ImageEditorViewModel();
+            this.DataContext = vm;
+            
         }
 
-        /// <summary>
-        /// Populates the page with content passed during navigation. Any saved state is also
-        /// provided when recreating a page from a prior session.
-        /// </summary>
-        /// <param name="sender">
-        /// The source of the event; typically <see cref="NavigationHelper"/>
-        /// </param>
-        /// <param name="e">Event data that provides both the navigation parameter passed to
-        /// <see cref="Frame.Navigate(Type, Object)"/> when this page was initially requested and
-        /// a dictionary of state preserved by this page during an earlier
-        /// session. The state will be null the first time a page is visited.</param>
+
+        private async void LoadBaseImage(StorageFile file)
+        {
+            var bitmap = new BitmapImage();
+            var stream = await file.OpenReadAsync();
+            await bitmap.SetSourceAsync(stream);
+            MainImage.Source = bitmap;
+        }
+
+
         private void navigationHelper_LoadState(object sender, LoadStateEventArgs e)
         {
         }
 
-        /// <summary>
-        /// Preserves state associated with this page in case the application is suspended or the
-        /// page is discarded from the navigation cache.  Values must conform to the serialization
-        /// requirements of <see cref="SuspensionManager.SessionState"/>.
-        /// </summary>
-        /// <param name="sender">The source of the event; typically <see cref="NavigationHelper"/></param>
-        /// <param name="e">Event data that provides an empty dictionary to be populated with
-        /// serializable state.</param>
+ 
         private void navigationHelper_SaveState(object sender, SaveStateEventArgs e)
         {
         }
 
         #region NavigationHelper registration
 
-        /// The methods provided in this section are simply used to allow
-        /// NavigationHelper to respond to the page's navigation methods.
-        /// 
-        /// Page specific logic should be placed in event handlers for the  
-        /// <see cref="GridCS.Common.NavigationHelper.LoadState"/>
-        /// and <see cref="GridCS.Common.NavigationHelper.SaveState"/>.
-        /// The navigation parameter is available in the LoadState method 
-        /// in addition to page state preserved during an earlier session.
 
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
             navigationHelper.OnNavigatedTo(e);
+            var file = e.Parameter as StorageFile;
+            LoadBaseImage(file);
         }
 
         protected override void OnNavigatedFrom(NavigationEventArgs e)
@@ -102,5 +91,16 @@ namespace imagesIPM
         }
 
         #endregion
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //close
+            navigationHelper.GoBack();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            //save
+        }
     }
 }
---
+++ imagesIPM/ViewModels/ImageEditorViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace imagesIPM.ViewModels
+{
+    class ImageEditorViewModel
+    {
+        string pathToImage;
+
+        public string PathToImage
+        {
+            get { return pathToImage; }
+            set { pathToImage = value; }
+        }
+
+        bool imgChanged = false;
+
+        public bool ImgChanged
+        {
+            get { return imgChanged; }
+            set { imgChanged = value; }
+        }
+    }
+}
